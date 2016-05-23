427a4e28c1e02e60249c220e1dc69fd2b1f870fb
Brandon Diaz-Abreu <brandon.diaz@rutgers.edu>
2016-05-23 16:55:48 -0400

    almost there. Had to fix the env.PORT

--- app.js
+++ app.js
@@ -285,6 +285,6 @@ server.get(/.*/, restify.serveStatic({
     'default': 'index.html'
 }));
 
-server.listen(process.env.port || 3978, function () {
+server.listen(process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
